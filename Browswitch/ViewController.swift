74366e9423cedea25ab9dfa3652a4fcbbad52620
slowbackspace <slowbackspace@gmail.com>
2019-03-01 21:18:06 +0100

    Initial Commit

---
+++ Browswitch/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Browswitch
+//
+//  Created by Maroš Špak on 01/03/2019.
+//  Copyright © 2019 slowbackspace. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
